004ee75f17edc9bb5cfef58ee3298ac45ebfc07e
BRISCOE, CHRISTOPHER W <chris.briscoe@fredmeyer.com>
2019-11-26 09:55:32 -0800

    wip

---
+++ sql/db2/sfp/magic_pid_desc_audit.sql
@@ -0,0 +1,17 @@
+
+    
+    
+    
+      select rmote.sku_no as sku_nbr
+      ,pdtco.con_dsc_tx as pid_pdtco_value
+      ,pdtca.cas_dsc_tx as pid_pdtca_value
+      ,rmote.itm_abb_dsc_tx as magic_short_desc
+      ,rmote.itm_dsc_tx as magic_long_desc
+  from prd.pid_pdtco pdtco
+      ,prd.pid_pdtca pdtca
+      ,prd.pid_rmote rmote
+ where pdtco.con_upc_no = rmote.con_upc_no
+   and pdtca.cas_upc_no = rmote.cas_upc_no
+   and rmote.sku_no in ('39030511', '59030515')
+  ;
+  
