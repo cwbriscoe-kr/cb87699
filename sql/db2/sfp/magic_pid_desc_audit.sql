a652d7521dd8b6aeef52d24d64c2882d131592ca
Chris Briscoe <chris.briscoe@fredmeyer.com>
2019-11-26 13:50:30 -0500

    wip

--- sql/db2/sfp/magic_pid_desc_audit.sql
+++ sql/db2/sfp/magic_pid_desc_audit.sql
@@ -1,8 +1,4 @@
-
-    
-    
-    
-      select rmote.sku_no as sku_nbr
+select rmote.sku_no as sku_nbr
       ,pdtco.con_dsc_tx as pid_pdtco_value
       ,pdtca.cas_dsc_tx as pid_pdtca_value
       ,rmote.itm_abb_dsc_tx as magic_short_desc
