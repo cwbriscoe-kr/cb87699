0d8efe700804c1c83add1bbed8e90d39f009cdc5
Briscoe <chris.briscoe@fredmeyer.com>
2020-06-05 16:38:38 -0400

    wip

---
+++ sql/db2/magic/skus_in_subclass.sql
@@ -0,0 +1,11 @@
+select is2.*
+  from accp.is2_itm_sku is2
+      ,accp.fi1_ft_itm fi1
+ where is2.itm_nbr = fi1.itm_nbr
+   and fi1.rec_stat_cd = '01'
+   and is2.rec_stat_cd = '30'
+   and is2.sku_typ_cd in ('01','02')
+   and fi1.ft_lvl09_cd = '945'
+   and sku_nbr < '10000000'
+ order by is2.sku_nbr
+   ;
