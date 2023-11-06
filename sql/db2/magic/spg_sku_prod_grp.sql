1b424fcb61bc19f7c34c5776965ece67c942f0e3
cwbriscoe <christopher.briscoe@kroger.com>
2023-11-06 07:14:39 -0800

    wip

---
+++ sql/db2/magic/spg_sku_prod_grp.sql
@@ -0,0 +1,5 @@
+select *
+  from prd.spg_sku_prod_grp sspg 
+ where prod_grp_ind != 'A'
+ with ur
+ ;
---
+++ sql/db2/magic/spl_ku_pg_loc.sql
@@ -0,0 +1,5 @@
+select *
+  from prd.spl_sku_pg_loc sspl 
+ where prod_grp_ind != 'P'
+ with ur
+ ;
